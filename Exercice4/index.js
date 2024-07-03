9f826f68578a7c652ad989cd99793167852d56dc
ybeb <younes.bouchi@gmail.com>
2024-07-03 10:19:52 +0200

    ajout du p boutton

--- Exercice4/index.js
+++ Exercice4/index.js
@@ -1,11 +1,14 @@
-let myButton=document.getElementById("myButton");
+let myButton = document.getElementById("myButton");
 
-myButton.addEventListener("click",)
+myButton.addEventListener("click");
 
-
-function clickEvent(){
-let body=
-
-
-
+function clickEvent() {
+  let body = document.querySelector("body");
+  let buttonParagraphId = document.getElementById("button-paragraph");
+  if (!buttonParagraphId) {
+    let buttonParagraph = document.createElement("p");
+    buttonParagraph.textContent = "Bonjour, vous avez cliqué sur le bouton !";
+    buttonParagraph.id="button-paragraph";
+    body.appendChild(buttonParagraph);
+  }
 }
